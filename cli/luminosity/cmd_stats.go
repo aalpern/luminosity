29134bb4e8dba84b9694f788c9cea32d0b761b70
Adam Alpern <adam.alpern@gmail.com>
2019-05-25 10:11:39 -0700

    Update stats command to recurse through directories as well as just listed catalogs

--- cli/luminosity/cmd_stats.go
+++ cli/luminosity/cmd_stats.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -11,22 +12,31 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	CatalogExtension        = "lrcat"
+	CatalogDataDirExtension = "lrdata"
+)
+
 func CmdStats(app *cli.Cli) {
 	app.Command("stats", "Generate catalog statistics", func(cmd *cli.Cmd) {
 
-		cmd.Spec = "[--outfile] [--per-catalog] CATALOG..."
+		cmd.Spec = "[--outfile] [--per-catalog] [--recursive] PATH..."
 
 		outfile := cmd.StringOpt("o outfile", "stats.json",
 			"Path to output file")
 		perCatalog := cmd.BoolOpt("p per-catalog", false,
 			"Output a summary .json file for each catalog, in addition to the merged output")
-		catalogs := cmd.StringsArg("CATALOG", nil,
-			"Catalogs to process")
+		recursive := cmd.BoolOpt("R recursive", false,
+			"Recurse into any directories include as PATH arguments")
+		paths := cmd.StringsArg("PATH", nil,
+			"Paths to process, which can be .lrcat files or directories")
 
 		cmd.Action = func() {
 			merged := luminosity.NewCatalog()
+			catalogPaths := findCatalogs(*recursive, (*paths)...)
+			var total int
 
-			for _, path := range *catalogs {
+			for _, path := range catalogPaths {
 				c, err := luminosity.OpenCatalog(path)
 				if err != nil {
 					log.WithFields(log.Fields{
@@ -36,6 +46,7 @@ func CmdStats(app *cli.Cli) {
 					}).Warn("Error opening catalog, skipping")
 					continue
 				}
+
 				err = c.Load()
 				if err != nil {
 					log.WithFields(log.Fields{
@@ -43,6 +54,7 @@ func CmdStats(app *cli.Cli) {
 						"catalog": path,
 						"error":   err,
 					}).Warn("Error loading catalog, skipping")
+					c.Close()
 					continue
 				}
 
@@ -51,12 +63,90 @@ func CmdStats(app *cli.Cli) {
 					write(jsPath, c)
 				}
 
+				total++
+				log.WithFields(log.Fields{
+					"action": "process_catalog",
+					"path":   path,
+					"status": "ok",
+				}).Info("Processed catalog")
+
 				merged.Merge(c)
+				c.Close()
 			}
 
 			write(*outfile, merged)
+
+			log.WithFields(log.Fields{
+				"action":             "status",
+				"status":             "complete",
+				"catalogs_processed": total,
+			}).Info("Complete")
 		}
 	})
+}
+
+func findCatalogs(recurse bool, paths ...string) []string {
+	found := make([]string, 0, len(paths))
+
+	// For each path in the input
+	for _, path := range paths {
+		info, err := os.Stat(path)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"action": "find_catalogs",
+				"status": "stat_error",
+				"path":   path,
+				"error":  "err",
+			}).Warn("Cannot stat path")
+			continue
+		}
+
+		// Process files
+		if !info.IsDir() {
+			if !strings.HasSuffix(path, CatalogExtension) {
+				log.WithFields(log.Fields{
+					"action": "find_catalogs",
+					"status": "wrong_suffix",
+					"path":   path,
+				}).Debug("Not a catalog file")
+			} else {
+				found = append(found, path)
+			}
+		} else {
+			// Process directories
+			children := findCatalogsInDir(recurse, path)
+			found = append(found, children...)
+		}
+	}
+	return found
+}
+
+func findCatalogsInDir(recurse bool, path string) []string {
+	found := make([]string, 0, 8)
+
+	if recurse {
+		filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.WithFields(log.Fields{
+					"action": "find_catalogs",
+					"status": "walk_error",
+					"path":   path,
+					"error":  "err",
+				}).Warn("Error walking path")
+			} else if !info.IsDir() {
+				found = append(found, findCatalogs(false, p)...)
+			} else if info.IsDir() {
+				// Skip the .lrdata directories which contain the
+				// potentially huge number of cached image previews
+				if strings.HasSuffix(p, CatalogDataDirExtension) {
+					return filepath.SkipDir
+				}
+			}
+			return nil
+		})
+	}
+
+	return found
 }
 
 func write(path string, data interface{}) {
